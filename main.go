af13108905527505467dac6d2cab84953ffa0d53
SloppySlime <114940023+2Sloppy4slime@users.noreply.github.com>
2025-11-21 11:43:43 +0100

    exo 4 partie 2
    distant site github :)

--- main.go
+++ main.go
@@ -15,3 +15,5 @@ func main() {
 	// to add : color functionality
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+//modification distante pour exo 4 partie 2 oulala je suis un employé qui peut pas bosser sur un autre fichier et qui complique la vie de tout le monde
